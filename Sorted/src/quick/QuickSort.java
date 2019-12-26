a991e934c09a4d640332ac1b3e5c8f1fc4e45fe5
wangpxp <wangpxp@gmail.com>
2019-12-26 23:03:43 +0800

    快速排序优化

--- Sorted/src/quick/QuickSort.java
+++ Sorted/src/quick/QuickSort.java
@@ -14,6 +14,7 @@ import static util.SortTestHelper.*;
  * 时间复杂度O(NlogN)，最差O(n^2)
  */
 public class QuickSort {
+    static Random random = new Random();
 
     private static void quickSort(Comparable[] arr) {
         quickSort(arr, 0, arr.length - 1);
@@ -22,26 +23,42 @@ public class QuickSort {
     private static void quickSort(Comparable[] arr, int l, int r) {
         if (l >= r) return; //递归终止条件
 
-        int p = partition(arr, l, r);
+        int p = partition_2(arr, l, r);
         quickSort(arr, l, p - 1);
         quickSort(arr, p + 1, r);
     }
 
+    // 改进的partition算法，让数组前后两端同时进行工作，可以让有着大量重复元素下的排序更快
+    // arr[l+1...i)<=v, arr(j...r]>=v
+    private static int partition_2(Comparable[] arr, int l, int r) {
+        swap(arr, l, random.nextInt(r - l + 1) + l);
+        Comparable v = arr[l];
+        int i = l + 1, j = r;
+        while (true) {
+            while (i <= r && less(arr[i], v)) i++; //i<=r可以省略，j>l也是
+            while (j > l && less(v, arr[j])) j--;
+            if (i >= j) break;
+            swap(arr, i++, j--);
+        }
+        swap(arr, l, j);
+        return j;
+    }
+
     // 返回p,使得arr[l...p-1]<arr[p],arr[p+1...r]>arr[p]
     private static int partition(Comparable[] arr, int l, int r) {
-        Random random = new Random();
         swap(arr, l, random.nextInt(r - l + 1) + l);
         Comparable v = arr[l]; // 通常选取数组第一个值作为基准点,但是如果数组近乎有序，效率会退化到O(N^2)
 
-        //对后面的数组遍历一遍
-        // arr[l+1...j]<v,arr[j+1...i)>v
-        int j = l; // 将j初始化的值设置为l
+        int j = l; // 初始值要定义好
+        // 对后面的数组遍历一遍, 使得arr[l+1..j]<v, arr[j...i] >v
         for (int i = l + 1; i <= r; i++) {
             if (less(arr[i], v)) swap(arr, ++j, i);
         }
         swap(arr, l, j);
         return j;
     }
+
+
 
     public static void main(String[] args) {
         Integer[] arr = generateArray(100, 0, 100);
