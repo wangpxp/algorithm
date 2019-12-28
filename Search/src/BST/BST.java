79d0747cf9098add06745035c151940810b2196d
wangpxp <wangpxp@gmail.com>
2019-12-28 22:06:30 +0800

    二分搜索树

--- Search/src/BST/BST.java
+++ Search/src/BST/BST.java
@@ -29,4 +29,28 @@ public class BST <E extends Comparable<E>> {
         return size == 0;
     }
 
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    private void add(Node node, E e) {
+        if (e.equals(node)) return;
+        else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        if (e.compareTo(node.e) < 0) add(node.left, e);
+        else if (e.compareTo(node.e) > 0) add(node.right, e);
+    }
+
 }
